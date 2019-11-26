56afa7f02a16ee65c1cdfbe2c93d193839331b80
Samuel Liu <samuel.liu1004@gmail.com>
2019-11-25 19:25:34 -0800

    Initial commit

---
+++ QuackLearners.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+
+//Defining servos
+Servo servo1;
+Servo servo2;
+
+//Servo position is degrees
+int servoPos = 0;
+int buttonPin;
+
+void setup()
+{
+  //Define servos signal inputs (Digital PWM 3-5)
+  buttonPin = 7;
+  pinMode(buttonPin, INPUT_PULLUP);
+  servo1.attach(3);
+  servo2.attach(5);
+}
+
+void loop()
+{
+  while(1){
+    if(digitalRead(buttonPin) == LOW){
+      servo1.write(0);
+      servo2.write(0);
+      for(servoPos = 0; servoPos < 180; servoPos++){
+        servo1.write(servoPos);
+        delay(5);
+      }
+      for(servoPos = 180; servoPos > 0; servoPos--){
+        servo1.write(servoPos);
+        delay(5);
+      }
+      for(servoPos = 0; servoPos < 180; servoPos++){
+        servo2.write(servoPos);
+        delay(5);
+      }
+      for(servoPos = 180; servoPos > 0; servoPos--){
+        servo2.write(servoPos);
+        delay(5);
+      }
+    }
+  }
+}
